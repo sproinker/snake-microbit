e5e040bbb399cbe9897ba75ee450450792562250
sproink <58812585+sproinker@users.noreply.github.com>
2024-11-13 10:21:04 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ snake = [0, 0]
 food = [randint(1, 4), randint(1, 4)]
 directions = [[1, 0], [0, -1], [-1, 0], [0, -1]]
 basic.clear_screen()
-
-while True:
-    
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    """)
--- main.ts
+++ main.ts
@@ -3,3 +3,10 @@ let snake = [0, 0]
 let food = [randint(1, 4), randint(1, 4)]
 let directions = [[1, 0], [0, -1], [-1, 0], [0, -1]]
 basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
