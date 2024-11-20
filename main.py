474ea4e52be7f31493091808328005aee2a5a63c
sproink <58812585+sproinker@users.noreply.github.com>
2024-11-20 22:33:16 +0700

    added food and movement code (no controls yet)

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 direction = 0
-snake = [0, 0]
+directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+snake = [[0, 0]]
 food = [randint(1, 4), randint(1, 4)]
-directions = [[1, 0], [0, -1], [-1, 0], [0, -1]]
-basic.clear_screen()
-basic.show_leds("""
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    """)
+
+while True:
+    basic.clear_screen()
+    led.plot_brightness(food[0], food[1], 255)
+    for i in range(len(snake)):
+        led.plot_brightness(snake[i][0], snake[i][1], 5 -(i / len(snake) * 5)%5 + 2)
+    basic.pause(800)
+
+    next_block = [(snake[0][0] + directions[direction][0]) % 5, (snake[0][1] + directions[direction][1]) % 5]
+    if next_block in snake:
+        basic.show_string("Game Over")
+        break
+    snake = [next_block] + snake
+    if next_block == food:
+        while food in snake:
+            food = [randint(0,4), randint(0,4)]
+    else:
+        snake.pop()
+
+    if len(snake) == 25:
+        basic.show_string("You Win")
+        break
--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
+let next_block: any[];
 let direction = 0
-let snake = [0, 0]
+let directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+let snake = [[0, 0]]
 let food = [randint(1, 4), randint(1, 4)]
-let directions = [[1, 0], [0, -1], [-1, 0], [0, -1]]
-basic.clearScreen()
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+while (true) {
+    basic.clearScreen()
+    led.plotBrightness(food[0], food[1], 255)
+    for (let i = 0; i < snake.length; i++) {
+        led.plotBrightness(snake[i][0], snake[i][1], 5 - i / snake.length * 5 % 5 + 2)
+    }
+    basic.pause(800)
+    next_block = [(snake[0][0] + directions[direction][0]) % 5, (snake[0][1] + directions[direction][1]) % 5]
+    if (snake.indexOf(next_block) >= 0) {
+        basic.showString("Game Over")
+        break
+    }
+    
+    snake = [next_block].concat(snake)
+    if (next_block == food) {
+        while (snake.indexOf(food) >= 0) {
+            food = [randint(0, 4), randint(0, 4)]
+        }
+    } else {
+        _py.py_array_pop(snake)
+    }
+    
+    if (snake.length == 25) {
+        basic.showString("You Win")
+        break
+    }
+    
+}
