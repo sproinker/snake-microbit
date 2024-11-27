4b51939d84ef6afdc890e601e59af05c56c7d440
sproink <58812585+sproinker@users.noreply.github.com>
2024-11-27 21:28:56 +0700

    actual controls

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
 snake = [[0, 0]]
 food = [randint(1, 4), randint(1, 4)]
 
+# snake movement buttons
 def on_button_single_clicked_a():
-    pass
+    global direction
+    direction = 3
 buttonClicks.on_button_single_clicked(buttonClicks.AorB.A, on_button_single_clicked_a)
 
 def on_button_double_clicked_a():
-    pass
+    global direction
+    direction = 0
 buttonClicks.on_button_double_clicked(buttonClicks.AorB.A, on_button_double_clicked_a)
 
 def on_button_single_clicked_b():
-    pass
+    global direction
+    direction = 2
 buttonClicks.on_button_single_clicked(buttonClicks.AorB.B, on_button_single_clicked_b)
 
 def on_button_double_clicked_b():
-    pass
+    global direction
+    direction = 1
 buttonClicks.on_button_double_clicked(buttonClicks.AorB.B, on_button_double_clicked_b)
 
 while True:
@@ -33,12 +38,22 @@ while True:
         break
     snake = [next_block] + snake
 
+    # score system
     if next_block == food:
         while food in snake:
             food = [randint(0,4), randint(0,4)]
+            if direction == 0:
+                snake.append([snake[0][0] - 1, snake[0][1] - 1])
+            elif direction == 1:
+                snake.append([snake[0][0], snake[0][1] - 1])
+            elif direction == 2:
+                snake.append([snake[0][0] + 1, snake[0][1] + 1])
+            elif direction == 3:
+                snake.append([snake[0][0] + 1, snake[0][1]])
     else:
         snake.pop()
 
+    # win condition
     if len(snake) == 25:
         basic.show_string("You Win")
         break
--- main.ts
+++ main.ts
@@ -3,17 +3,22 @@ let direction = 0
 let directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
 let snake = [[0, 0]]
 let food = [randint(1, 4), randint(1, 4)]
+//  snake movement buttons
 buttonClicks.onButtonSingleClicked(buttonClicks.AorB.A, function on_button_single_clicked_a() {
     
+    direction = 3
 })
 buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function on_button_double_clicked_a() {
     
+    direction = 0
 })
 buttonClicks.onButtonSingleClicked(buttonClicks.AorB.B, function on_button_single_clicked_b() {
     
+    direction = 2
 })
 buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.B, function on_button_double_clicked_b() {
     
+    direction = 1
 })
 while (true) {
     basic.clearScreen()
@@ -29,14 +34,26 @@ while (true) {
     }
     
     snake = [next_block].concat(snake)
+    //  score system
     if (next_block == food) {
         while (snake.indexOf(food) >= 0) {
             food = [randint(0, 4), randint(0, 4)]
+            if (direction == 0) {
+                snake.push([snake[0][0] - 1, snake[0][1] - 1])
+            } else if (direction == 1) {
+                snake.push([snake[0][0], snake[0][1] - 1])
+            } else if (direction == 2) {
+                snake.push([snake[0][0] + 1, snake[0][1] + 1])
+            } else if (direction == 3) {
+                snake.push([snake[0][0] + 1, snake[0][1]])
+            }
+            
         }
     } else {
         _py.py_array_pop(snake)
     }
     
+    //  win condition
     if (snake.length == 25) {
         basic.showString("You Win")
         break
